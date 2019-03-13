fea7a7e0e421d271b6a7e00cc38823c15e4668ac
Non Dewman <Dnewman1934@yahoo.com>
2019-03-13 16:46:33 -0500

    everything works but the reload

--- Assets/sharkscript.js
+++ Assets/sharkscript.js
@@ -1,23 +1,31 @@
-let guessVar=10, winCount=0, lossCount=0, rightGuess=0,wrongGuess=[], underscores=''; //Establish variables
-const wordChoiceList = ["dorsal", "gill", "tailfin", "snout", "spiracle", "mako"];//Word choice list
+let guessVar=10, rightGuess=0,wrongGuess=[], underscores=''; //Establish variables
+let winCount=0;
+localStorage.setItem("winItem", winCount);
+let lossCount=0;
+localStorage.setItem("lossItem", lossCount);
+const wordChoiceList = ["tiger", "bull", "wobbegong", "carpet", "hammerhead", "mako", "nurse"];//Word choice list
 let currentWord=wordChoiceList[Math.floor(Math.random() * wordChoiceList.length)]; //random selection
 cwLength=currentWord.length;
 newUnderscores="";
-console.log(currentWord); //show word for guess in console
+console.log(currentWord);
 for (i = 0; i < cwLength ; i++) {
-          var x = currentWord.charAt(i);
-          if(x === " ")
-         {
-            underscores += x;
-          }
-          else 
-          {
-            underscores += "_";
-          }
-        }                                                           //Replace letters with underscores
+  var x = currentWord.charAt(i);
+  if(x === " ")
+ {
+    underscores += x;
+  }
+  else 
+  {
+    underscores += "_";
+  }
+}                      
+let wordArray=underscores.split("");                      //Replace letters with underscor
+  
+function hangMan(){
 document.onkeyup = function(event) {  //when key pressed
   let userGuess = event.key.toLowerCase(); //assign the input to userGuess
-  let resultVar = currentWord.search(userGuess); //sets resultVar to if the letter is in word or not
+  let resultVar = currentWord.search(userGuess);
+  document.getElementById('directionsText').innerHTML=(''); //sets resultVar to if the letter is in word or not
     if (resultVar > -1) //if it is,
     {
       rightGuess++; //add one to rightguess
@@ -25,33 +33,37 @@ document.onkeyup = function(event) {  //when key pressed
             {
               if (userGuess === currentWord.charAt(i)) //if the input is in current word,
                 {
-                  let newUnderscores=underscores.replace(underscores.charAt(i), userGuess);
-                  console.log(i);
-                  console.log(newUnderscores)
-                let hiddenWord = document.getElementById("currentWord");
-                 hiddenWord.innerHTML = 'your current word is'+newUnderscores;
+                  wordArray[i] = currentWord.charAt(i);
+                 console.log(wordArray);
+                 let hiddenWord = document.getElementById("currentWord");
+                 hiddenWord.innerHTML = 'your current word is '+wordArray;
                 }
-                //if (newUnderscores === currentWord) //if all the underscores have become letters,
-                //{
-                  //document.write('YOU WIIIIN! (: your word was "'+currentWord+'. Press any key to start over'); //victory message
-                  //document.onkeyup= function(event) //reload page on keyup
-                    //{
-                    //location.reload();
-                    //}
-                //}
-            }
-          }
-    
+               } if (wordArray.join("")===currentWord){
+                    document.write('YOU WIIIIN! (: your word was "'+currentWord+'. Press any key to start over');
+                    winCount++
+                     //victory message
+                    document.onkeyup= function(event) //reload page on keyup
+                      {
+                         location.reload();
+                      }
+                  }
+                }
         else { //runs if input is incorrect
                 guessVar--; //minus one guess
                 wrongGuess.push(userGuess); //add to empty array of wrong input
                 document.getElementById('incorrectGuesses').innerHTML = ('Wrong letters guessed: '+wrongGuess); //displays info
                 document.getElementById('guessCount').innerHTML = ('Guesses left: ' + guessVar); 
                 if (guessVar === 0){ //if you run out of guesses
-                    document.write('YOU LOSE! Press any key to start over'); //loss screen
+                    document.write('YOU LOSE! Press any key to start over'); 
+                    lossCount++//loss screen
                     document.onkeyup= function(event){
                     location.reload();
                 }
                 }
               }
             }
+}
+document.onload=hangMan();
+document.getElementById("currentWord").innerText=('Your word is '+wordArray);
+document.getElementById("wincount").innerText=('Wins: '+winCount);
+document.getElementById("losstext").innerText=('losses: '+lossCount);
